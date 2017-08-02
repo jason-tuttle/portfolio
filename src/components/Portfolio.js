b676dfa338aa68ad5ddbbdc96fd129a71e14233a
Jason Tuttle <jason@hearspotbark.com>
2017-08-02 15:44:39 -0400

    added new portfolio piece

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import game from '../images/game_of_life.png';
 import rover from '../images/mars_rover2.jpg';
+import fretboard from '../images/fretboard.png';
 
 class Portfolio extends Component {
 
@@ -11,12 +12,16 @@ class Portfolio extends Component {
         <div>
           <p>You can find a lot in <a href="https://github.com/jason-tuttle">my GitHub</a> repositories:</p>
           <div style={{width: '80vw', margin:'auto'}}>
+            <img src={fretboard} style={{width: '40vw'}} alt='fret board: mock store-front' /><br />
+            <p>Fret Board, a mock store-front, written with React and Redux: <a href="https://jason-tuttle.github.io/week-10/">See it here</a></p>
+          </div>
+          <div style={{width: '80vw', margin:'auto'}}>
             <img src={game} style={{width: '40vw'}} alt='game of life simulation' /><br />
             <p>Conway's Game of Life simulation, written with React: <a href="https://jason-tuttle.github.io/game-of-life/">Run it here</a></p>
           </div>
           <div style={{width: '80vw', margin:'auto'}}>
             <img src={rover} style={{width: '40vw'}} alt='mars rover browser' /><br />
-            <p>Pull photos from NASA Mars rovers, written with React: <a href="https://jason-tuttle.github.io/rover-photos/">Run it here</a></p>
+            <p>Pull photos from NASA Mars rovers, written with React: <a href="https://jason-tuttle.github.io/rover-photos/">See it here</a></p>
           </div>
         </div>
 
