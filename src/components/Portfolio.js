0a2d6841a5815ad8a3f5b639b054a56d85727bd2
Jason Tuttle <jason@hearspotbark.com>
2017-08-02 21:01:37 -0400

    adding sites

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import game from '../images/game_of_life.png';
 import rover from '../images/mars_rover2.jpg';
 import fretboard from '../images/fretboard.png';
+import wordgame from '../images/word_game.png';
+import filmclub from '../images/filmclub.png';
 
 class Portfolio extends Component {
 
@@ -22,6 +24,15 @@ class Portfolio extends Component {
           <div style={{width: '80vw', margin:'auto'}}>
             <img src={rover} style={{width: '40vw'}} alt='mars rover browser' /><br />
             <p>Pull photos from NASA Mars rovers, written with React: <a href="https://jason-tuttle.github.io/rover-photos/">See it here</a></p>
+          </div>
+          <div style={{width: '80vw', margin:'auto'}}>
+            <img src={filmclub} style={{width: '40vw'}} alt='film club mock site' /><br />
+            <p>Responsive design assignment, written with just HTML &amp; CSS: <a href="https://jason-tuttle.github.io/tiy-week-two-project/">See it here</a></p>
+          </div>
+
+          <div style={{width: '80vw', margin:'auto'}}>
+            <img src={wordgame} style={{width: '40vw'}} alt='mystery word game' /><br />
+            <p>Full-Stack web app, Node.js/Express back-end, front-end rendered in Mustache: <a href="https://mystery-word-game.herokuapp.com/game">See it here</a></p>
           </div>
         </div>
 
