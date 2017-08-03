27a152b696369da61de1e66b65333291d8b05718
Jason Tuttle <jason@hearspotbark.com>
2017-08-02 21:14:33 -0400

    about content

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,18 @@ class About extends Component {
           Award-winning sound editor!<br />
           Mostly harmless.<br />
         </p>
-        <p>There's not enough here, yet, so there will be more to come in the future. Stay tuned!</p>
+        <p>After nearly 20 years working in sound &mdash; <br />
+        editing sound for video games, films, &amp; television &mdash;<br />
+        mixing live sound for theatre &amp; music &mdash;<br />
+        &mdash;<br />
+        I'm adding web development to that list.<br />
+        12 weeks at the Iron Yard in Indianapolis has prepared me for a lifetime of learning to code,<br />
+        because if I've learned one thing, it's that I haven't learned <em>anything</em> yet... in the grand scheme of things.
+      </p>
+      <p>However, I <em>have</em> learned that I can learn, and pretty fast, too.<br />
+      HTML, CSS, &amp; JavaScript, with Node.js/Express, SQL(Postgres), MongoDB/Mongoose, &amp; React/React Native to round it all out.</p>
+      <p>Like I said, what do <em>you</em> want to do today?</p>
+      <p><i className="fa fa-globe fa-3x" ></i></p>
       </div>
     );
   }
